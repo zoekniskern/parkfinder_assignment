cd1a5ba564255225e2807e4453cac2ad679559f6
Student <zxk3766@rit.edu>
2019-03-23 15:26:19 -0400

    xcode project base

---
+++ Kniskern_ParkFinder/Kniskern_ParkFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kniskern_ParkFinder
+//
+//  Created by Student on 3/23/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
